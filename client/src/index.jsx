066c88f23a4f46d1b6e1ff1d9fe7ccc0003d1359
AmyGrimm131326 <amygrimm2004@gmail.com>
2025-05-31 17:55:15 -0400

    Made changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+// In src/index.js or App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 ReactDOM.createRoot(document.querySelector('#root')).render(<App/>)
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 // Setting up my database page
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/weaverFlooring',{
+mongoose.connect('mongodb://127.0.0.1:27017/weaverFlooring',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }
